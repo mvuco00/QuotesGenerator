a9fd4dd6be858d91e52e6b72f8497c2e10e37f30
mvuco00 <mvuco00@fesb.hr>
2021-03-13 21:15:47 +0100

    all quotes pagination

--- quotesgenerator/src/components/AllQuotes.js
+++ quotesgenerator/src/components/AllQuotes.js
@@ -1,22 +1,51 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import ReactPaginate from "react-paginate";
 import { getQuotes } from "../store/actions";
 import AllQuotesQuote from "./AllQuotesQuote";
 
 const AllQuotes = () => {
   const allQuotes = useSelector((state) => state.quotes);
+  const [quotes, setQuotes] = useState(allQuotes.slice(0, 4000));
+  const [pageNumber, setPageNumber] = useState(0);
+
   const dispatch = useDispatch();
+  const quotesPerPage = 50;
+  const pagesVisited = pageNumber * quotesPerPage;
 
   useEffect(() => {
     dispatch(getQuotes());
   }, [dispatch]);
+  // za slice-anje json objekta kako se pomicemo po stranicama
+  const displayQuotes = quotes.slice(
+    pagesVisited,
+    pagesVisited + quotesPerPage
+  );
 
-  console.log(allQuotes);
+  // ceil->round up
+  const pageCount = Math.ceil(quotes.length / quotesPerPage);
+  // selected je broj stranice na koji zelimo ici, react-paginate sve rijesi
+  const changePage = ({ selected }) => {
+    setPageNumber(selected);
+  };
+
+  console.log(quotes);
   return (
-    <div>
-      {allQuotes.map((quote) => {
-        return <AllQuotesQuote quoteText={quote.text} />;
+    <div className="allquotescontainer">
+      {displayQuotes.map((quote, index) => {
+        return <AllQuotesQuote key={index} quoteText={quote.text} />;
       })}
+      <ReactPaginate
+        previousLabel={"Previous"}
+        nextLabel={"Next"}
+        pageCount={pageCount}
+        onPageChange={changePage}
+        containerClassName={"paginationButtons"}
+        previousLinkClassName={"previousButton"}
+        nextLinkClassName={"nextButton"}
+        disabledClassName={"paginationDisabled"}
+        activeClassName={"paginationActive"}
+      />
     </div>
   );
 };
--- quotesgenerator/src/components/AllQuotesQuote.js
+++ quotesgenerator/src/components/AllQuotesQuote.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 const AllQuotesQuote = ({ quoteText }) => {
-  console.log(quoteText);
   return <div className="allQuotesQuote">{quoteText}</div>;
 };
 
